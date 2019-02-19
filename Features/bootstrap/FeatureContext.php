ee24c2737973263dab06621cf8564d40e81c70fe
JAMES K ARASIM <jaemzware@hotmail.com>
2019-02-18 19:57:43 -0800

    made test more interesting

--- Features/bootstrap/FeatureContext.php
+++ Features/bootstrap/FeatureContext.php
@@ -5,18 +5,37 @@ use Behat\MinkExtension\Context\MinkContext;
 class FeatureContext extends MinkContext
 {
     /**
-     * @Then /^I wait for the suggestion box to appear$/
-     */
-    public function iWaitForTheSuggestionBoxToAppear()
-    {
-        $this->getSession()->wait(5000, "$('.suggestions-results').children().length > 0");
-    }
-
-    /**
      * @Given I wait for :arg1 seconds
      */
     public function iWaitForSeconds($arg1)
     {
         sleep($arg1);
     }	
+    
+    /**
+     * @Given I click on the smartsearch drop option :arg1
+     */
+    public function iClickOnTheSmartsearchDropOption($arg1)
+    {
+        $xpathOfDropdownOption = "//div[@class='option_name' and contains(text(),'".$arg1."')]";
+        $this->getSession()->wait(5000, $this->getSession()->getPage()->find('xpath',$xpathOfDropdownOption)===null?false:true);
+        $this->getSession()->getPage()->find('xpath',$xpathOfDropdownOption)->click();
+        $this->getSession()->wait(5000, $this->getSession()->getPage()->find('xpath',$xpathOfDropdownOption)===null?true:false);
+    }
+    
+    /**
+     * @Given I click the element with xpath :arg1
+     */
+    public function iClickTheElementWithXpath($arg1)
+    {
+        $this->getSession()->getPage()->find('xpath',$arg1)->click();
+    }
+    
+     /**
+     * @When I wait for the url to contain :arg1
+     */
+    public function iWaitForTheUrlToContain($arg1)
+    {
+        $this->getSession()->wait(5000,strpos($this->getSession()->getCurrentUrl(),$arg1));
+    }
 }
--- Features/fake.feature
+++ Features/fake.feature
@@ -1,7 +1,10 @@
 Feature: File for running tests
 
 @javascript
-Scenario: Go to a web page
+Scenario: Verify the real time arrivals page contains a map
 	Given I am on "/"
         And I fill in "arrivalsAddress1" with "3504 sw webster st"
-	And I wait for "5" seconds
+        And I click on the smartsearch drop option "3504 SW Webster St, Seattle, WA 98126"
+        When I click the element with xpath "//button[@data-role='real-time-arrivals']"
+        And I wait for the url to contain "real-time"
+        Then I should see a "#block-mapblock" element
